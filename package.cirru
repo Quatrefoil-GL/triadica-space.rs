8a6f8856f8aee2464274f452fb3068b6b6190989
tiye <jiyinyiyong@gmail.com>
2022-05-13 21:56:46 +0800

    try to load touch-control plugin

---
+++ package.cirru
@@ -0,0 +1,6 @@
+
+{}
+  :package |control
+  :modules $ [] |touch-control/compact.cirru
+  :init-fn |control.core/main!
+  :reload-fn |control.core/main!
--- web/main.mjs
+++ web/main.mjs
@@ -1,9 +1,11 @@
 import init, { initApp, onWindowResize } from "../pkg/triadica_space";
+import { main_$x_ } from "../js-out/control.core.mjs";
 
 window.onload = () => {
   init().then(() => {
     initApp();
   });
+  main_$x_();
 };
 
 window.addEventListener("resize", (event) => {
